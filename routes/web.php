bc43f52af4a1ac738da7b0950d384aa3118dbef5
HilmyMhiii <hilmymhi874@gmail.com>
2022-02-23 12:14:22 +0700

    Muhammad Hilmy Iqbal_02_praktikum_web_lanjut

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo ("Selamat Datang");
+});
+
+Route::get('/about', function (){
+    echo("Muhammad Hilmy Iqbal <br> 2041720129 <br> TI-2D");
+});
+
+Route::get('/artikel/1', function (){
+    echo("Halaman Artikel ID 1");
+});
+
+Route::get('/artikel/2', function (){
+    echo("Halaman Artikel ID 2");
+});
